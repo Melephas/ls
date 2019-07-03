d34767196da18bd5a1d283b2f16208cfa50b0bb8
Sam Miller <noogii7@gmail.com>
2019-07-03 10:19:55 +0100

    Changed multi-column output to single column output for stability

--- ls.go
+++ ls.go
@@ -23,21 +23,29 @@ func main() {
 
 	writer := tabwriter.NewWriter(color.Output, 0, 0, 3, ' ', tabwriter.DiscardEmptyColumns)
 	printBlue := color.New(color.FgHiBlue).SprintFunc()
+	printGreen := color.New(color.FgHiGreen).SprintFunc()
 
 	var stringBuilder strings.Builder
 
-	for x, entry := range contents {
+	for _, entry := range contents {
+
+		strings.TrimSuffix(entry.Name(), ".exe")
+
 		if entry.IsDir() {
 			stringBuilder.WriteString(printBlue(entry.Name()))
-			stringBuilder.WriteString("/\t")
+			stringBuilder.WriteString("/\t\n")
+		} else if strings.HasSuffix(entry.Name(), ".exe") {
+			stringBuilder.WriteString(printGreen(strings.TrimSuffix(entry.Name(), ".exe")))
+			stringBuilder.WriteString("\t\n")
 		} else {
 			stringBuilder.WriteString(entry.Name())
-			stringBuilder.WriteString("\t")
+			stringBuilder.WriteString("\t\n")
 		}
-
-		if x%4 == 0 && x > 0 {
-			stringBuilder.WriteRune('\n')
-		}
+		/*
+			if x%4 == 0 && x > 0 {
+				stringBuilder.WriteRune('\n')
+			}
+		*/
 	}
 
 	fmt.Fprint(writer, stringBuilder.String())
